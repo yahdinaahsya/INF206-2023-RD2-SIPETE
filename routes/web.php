2859bdf4269c920e70d6af5967a9a766dc373fd1
Muhammad Nizar Asykary <nizar_as@mhs.unsyiah.ac.id>
2023-04-13 09:25:08 +0700

    feat HalamanJual

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/halamanjual', function () {
+    return view('halamanjual');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');

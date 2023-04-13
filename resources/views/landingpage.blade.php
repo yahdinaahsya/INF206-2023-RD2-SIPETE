f75a1fee34fa998734284acb81dacbf2a273f85a
Riyanfr <riyan_fr@mhs.unsyiah.ac.id>
2023-04-13 16:21:13 +0700

    feat: landing bagian terakhir

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -20,13 +20,25 @@
 
 <div class="text-center pt-32">
     <div>
-        <h1 class=" font-[arial] font-extrabold text-2xl mb-9 text-[#118EEA]">About<span class="font-[arial] font-extrabold text-[#000000]"> Us</span></h1>
+        <h1 class=" font-[arial] font-extrabold text-4xl mb-9 text-[#118EEA]">About<span class="font-[arial] font-extrabold text-[#000000]"> Us</span></h1>
         <h2 class=" font-[arial] font-extrabold tracking-wide ">Kami ada 24 jam non-stop untuk melayani anda dalam menangani sampah tekstil</h2>
     </div>
     <div class="pt-16">
-        <img class="h-auto max-w-full mx-auto" src="images/pengolahan.jpg">
+        <img class="h-screen max-w-full mx-auto" src="images/pengolahan.jpg">
     </div>
 </div>
-
+<div>
+    <div class="text-center pt-20">
+        <h1 class="font-[arial] font-extrabold text-4xl">Informasi</h1>
+    </div>
+    <div class=" pt-16">
+        <img class="h-screen max-w-7xl mx-auto" src="images/pengolahan1.jpg">
+        <div class="h-24 mx-16 pb- bg-gradient-to-r from-cyan-500 to-blue-500">
+        <div class="flex pt-16">
+            <img class="h-auto max-w-lg pl-16" src="images/pengolahan2.jpg">
+            <img class="h-auto max-w-lg ml-40 " src="images/pengolahan3.jpg">
+        </div>
+    </div>
+</div>
 
 @endsection

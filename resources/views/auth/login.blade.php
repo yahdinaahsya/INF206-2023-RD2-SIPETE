08f85c4eab8babd69fb1753e15e071a0dff54f91
yahdinaahsya <yahdinakyeopta74@gmail.com>
2023-04-24 23:12:31 +0700

    fix desain halaman login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,68 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body style="font-family: Poppins" class="text-gray-900 antialiased">
+        <div class="min-h-screen flex flex-col sm:justify-center pt-6 sm:pt-0 bg-gray-100">
+            <div class="grid md:grid-cols-5 grid-cols-1 mt-20 md:mt-0">
+                <div class="hidden md:block col-span-2 h-screen rounded-r bg-[#0B63F8] p-16 justify-center">
+                    <p class="text-8xl pb-10 font-extrabold text-white">Halo!</p>
+                    <p class="text-2xl font-semibold text-white">Selamat datang kembali</p>
+                    
+                    <div class="mt-2 text-sm font-medium flex text-white justify-between">
+                        Ayo, tetap terhubung bersama kami ciptakan lingkungan bebas limbah tekstil!
+                    </div>
+                    <div class="flex justify-center h-auto">
+                        <img class="rounded-xl" src="{{('images/login.png') }}" style="transform: scaleX(-1);">
+                    </div>
+                </div>
+                <div  class="col-span-3">
+                    <div class="h-full grid grid-cols-1 content-center justify-center">
+                        <div class="flex justify-center">
+                            <form method="POST" action="{{ route('login') }}" class="md:w-1/2 w-full mx-5 grid grid-cols-1 gap-3">
+                                @csrf
+                                <div class="flex justify-center">
+                                    <img src="{{ asset('images/logo.svg') }}">
+                                </div>
+
+                                <input type="email" name="email" id="input-email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Email">
+                                <input type="password" name="password" id="input-password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Password">
+
+                                <div class="flex justify-first">
+                                    <!-- Remember Me -->
+                                    <div class="block mt-4">
+                                        <label for="remember_me" class="inline-flex items-center">
+                                            <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                                            <span class="ml-2 text-sm text-gray-600">{{ __('Ingat Saya') }}</span>
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <div class="flex justify-center">
+                                    <button type="submit" class="px-4 py-3 bg-[#0B63F8] text-white font-medium md:w-1/2 w-full rounded">Masuk</button>
+                                </div>
+
+                                <div class="flex justify-center">
+                                    <a href="{{ url('/register') }}" class=" text-xs font-medium text-[#0B63F8] hover:text-black">{{__('Belum buat akun? Daftarkan akun mu disini!')}}</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </body>
+</html>

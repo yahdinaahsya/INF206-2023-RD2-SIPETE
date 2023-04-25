91aaf20aa0ff23859e2994b8e6011ee4c8fe0dd1
Riyanfr <riyan_fr@mhs.unsyiah.ac.id>
2023-04-26 00:25:03 +0700

    feat: lambang tukar pulsa

--- resources/views/redeemkoin.blade.php
+++ resources/views/redeemkoin.blade.php
@@ -2,6 +2,28 @@
 
 @section ('content')
 
-    
+    <div class="py-auto px-auto my-auto mx-auto ">
+
+        <!-- Tukar Pulsa -->
+        <div class=" flex pt-24 ">
+            <div>
+                <img class="" src="/images/Ellipse 10.jpg">
+            </div>
+            <div class="absolute"> 
+                <img class="relative pt-7 pl-16 "  src="/images/Frame 1.jpg">
+            </div>
+            <div class="h-auto max-w-lg ml-auto pr-16 pt-3">
+                <img class="relative" src="/images/Close_square_fill.png">
+            </div>
+        </div>
+
+        <div>
+
+        </div>>
+
+
+
+    </div>
+
 
 @endsection

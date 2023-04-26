0f29648882fd4d62fb06b280f13c7c492f4d6ddf
Riyanfr <riyan_fr@mhs.unsyiah.ac.id>
2023-04-26 22:23:57 +0700

    feat: udah siap lambang sosmed

--- resources/views/redeemkoin.blade.php
+++ resources/views/redeemkoin.blade.php
@@ -7,10 +7,10 @@
         <!-- Tukar Pulsa -->
         <div class="flex pt-24 ">
             <div>
-                <img class="" src="/images/Ellipse 10.jpg">
+                <img  class=" h-64 w-auto" src="/images/Ellipse 10.jpg">
             </div>
             <div class="absolute"> 
-                <img class="relative pl-16 pt-7 "  src="/images/Frame 1.jpg">
+                <img class="relative pl-16 pt-7 "  src="/images/tukarpulsa.jpg">
             </div>
             <div class="h-auto max-w-lg pt-3 pr-16 ml-auto">
                 <img class="relative" src="/images/Close_square_fill.png">
@@ -19,14 +19,28 @@
 
         <div class="text-center ">
             <h1 class="font-extrabold text-8xl text-slate-700">Poin Sipete</h1>
-            <h1 class=" font-[serif]  font-extrabold text-8xl mb-9 text-[#118EEA]">$<span
-                class=" font-extrabold text-[#47A7EF]">100</span> </h1>
+            <h1 class=" font-[serif]  font-extrabold text-8xl mb-9 text-[#118EEA]">$<span 
+                class=" font-extrabold text-[#47A7EF]">100</span> </h1> <!--poin dan uang dolar-->
 
-                <div>
+                <div class=""> <!--tukar poin dan lambangnya-->
                     <h1 class="text-2xl font-extrabold text-slate-700">Tukar Poin Menjadi</h1>
-                    <h1 class="font-extrabold text-1xl mb-9 text-slate-700">Pulsa<span
+                    <h1 class="font-extrabold text-1xl  text-slate-700">Pulsa<span
                         class="font-extrabold pl-14">Token Listrik</span> </h1>
+                        <div class="inline-flex pr-7  ">
+                            <img class="pr-24 -mr-10" src="/images/pulsa-removebg-preview 1.jpg">
+                            <img class="pl-4  " src="/images/tokenListrik-removebg-preview 1.jpg">
+                        </div>
+                    <h1 class="font-extrabold pr-9">Klik</h1>
                 </div>
+        </div>
+
+        <div class="flex">
+            <div>
+                <img class="h-56 w-screen max-w-full" src="/images/Rectangle 4.png">
+            </div>
+            <div class="absolute pt-20 pl-96">
+                <img class=" relative h-14 pl-96 ml-56" src="/images/lambangsosmed.jpg">
+            </div>
         </div>
 
 

8c40ddcb8301dcff898971c9bc14577ab750bd81
Riyanfr <riyan_fr@mhs.unsyiah.ac.id>
2023-04-27 23:29:48 +0700

    feat: buat profil nizar

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,118 +1,121 @@
 @extends ('layouts.app')
 
 @section('content')
-<div class="mx-auto my-auto py-auto px-auto ">
-
-    <!--foto yahdina-->
-    <div class="h-screen -mb-56  bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
-        <div class="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
-            <div class=" h-56 overflow-hidden -mb-24 " >
-                <img class="" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"/>
-            </div>
-            <div class="flex justify-center px-5 ">
-                <img class="h-32 w-56 bg-white p-2 rounded-full   " src="/images/shputin.jpeg"  />
-            </div>
-
-            <div class=" ">
-                <div class="text-center px-14">
-                    <h2 class="font-serif text-gray-800 text-4xl font-bold">Yahdina Ahsya</h2>
-                    <h2 class="text-gray-700 text-2xl font-bold">(2108107010055)</h2>
-                    <a class="font-bold text-gray-600 mt-2 hover:text-blue-500" href="https://www.instagram.com/yahdina_a/" target="BLANK()">@yahdina_a</a>
-                    <p class="font-serif mt-2 text-gray-700 font-semibold text-sm">Hidup seperti larry</p>
+<div class="mx-auto my-auto py-auto px-auto">
+    <div class="-mt-6">
+        <!--foto yahdina-->
+        <div class="h-screen -mb-56  bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
+            <div class="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
+                <div class=" h-56 overflow-hidden -mb-24 " >
+                    <img class="" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"/>
+                </div>
+                <div class="flex justify-center px-5 ">
+                    <img class="h-32 w-56 bg-white p-2 rounded-full   " src="/images/shputin.jpeg"  />
+                </div>
+    
+                <div class=" ">
+                    <div class="text-center px-14">
+                        <h2 class="font-serif text-gray-800 text-4xl font-bold">Yahdina Ahsya</h2>
+                        <h2 class="text-gray-700 text-2xl font-bold">(2108107010055)</h2>
+                        <a class="font-bold text-gray-600 mt-2 hover:text-blue-500" href="https://www.instagram.com/yahdina_a/" target="BLANK()">@yahdina_a</a>
+                        <p class="font-serif mt-2 text-gray-700 font-semibold text-sm">Hidup seperti larry</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
-    <!--foto nitiya dan ilmi-->
-    <div class="h-screen -mb-56  bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
-        <!--foto nitiya-->
-        <div class="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
-            <div class=" h-56 overflow-hidden -mb-24 " >
-                <img class="" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"/>
-            </div>
-            <div class="flex justify-center px-5 ">
-                <img class="h-32 w-56 bg-white p-2 rounded-full" src="/images/shputin.jpeg"  />
-            </div>
-
-            <div class=" ">
-                <div class="text-center px-14">
-                    <h2 class="font-serif text-gray-800 text-3xl font-bold">Nitiya Rihadatul ‘Aisy</h2>
-                    <h2 class="text-gray-700 text-2xl font-bold">(2108107010003)</h2>
-                    <a class="font-bold text-gray-600 mt-2 hover:text-blue-500" href="https://www.instagram.com/nitiya_rihaisy/" target="BLANK()">@nitiya_rihaisy</a>
-                    <p class="font-serif mt-2 text-gray-700 font-semibold text-sm -mb-2">Kalo bisa jalan kenapa harus duduk</p>
-                    <p class="font-serif mt-2 text-gray-700 font-semibold text-sm">Kalo bisa dua kenapa harus satu</p>
+    
+        <!--foto nitiya dan ilmi-->
+        <div class="h-screen  -mb-56  bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
+            <!--foto nitiya-->
+            <div class="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
+                <div class=" h-56 overflow-hidden -mb-24 " >
+                    <img class="" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"/>
+                </div>
+                <div class="flex justify-center px-5 ">
+                    <img class="h-32 w-56 bg-white p-2 rounded-full" src="/images/shputin.jpeg"  />
+                </div>
+    
+                <div class=" ">
+                    <div class="text-center px-14">
+                        <h2 class="font-serif text-gray-800 text-3xl font-bold">Nitiya Rihadatul ‘Aisy</h2>
+                        <h2 class="text-gray-700 text-2xl font-bold">(2108107010003)</h2>
+                        <a class="font-bold text-gray-600 mt-2 hover:text-blue-500" href="https://www.instagram.com/nitiya_rihaisy/" target="BLANK()">@nitiya_rihaisy</a>
+                        <p class="font-serif mt-2 text-gray-700 font-semibold text-sm -mb-2">Kalo bisa jalan kenapa harus duduk</p>
+                        <p class="font-serif mt-2 text-gray-700 font-semibold text-sm">Kalo bisa dua kenapa harus satu</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        
-        <!--foto ilmi-->
-        <div class="container ml-20 lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
-            <div class=" h-56 overflow-hidden -mb-24 " >
-                <img class="" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"/>
-            </div>
-            <div class="flex justify-center px-5 ">
-                <img class="h-32 w-56 bg-white p-2 rounded-full" src="/images/shputin.jpeg"  />
-            </div>
-
-            <div class=" ">
-                <div class="text-center px-14">
-                    <h2 class="font-serif text-gray-800 text-4xl font-bold">Khairil Ilmi</h2>
-                    <h2 class="text-gray-700 text-2xl font-bold">(2108107010011)</h2>
-                    <a class="font-bold text-gray-600 mt-2 hover:text-blue-500" href="https://www.instagram.com/immohitdhiman/" target="BLANK()">@bg_putin.official</a>
-                    <p class="font-serif mt-2 text-gray-700 font-semibold text-sm">sesungguhnya berkasih lah di antara manusia perindah segala kata kata bahagia informatika</p>
+            
+            <!--foto ilmi-->
+            <div class="container ml-20 lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
+                <div class=" h-56 overflow-hidden -mb-24 " >
+                    <img class="" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"/>
+                </div>
+                <div class="flex justify-center px-5 ">
+                    <img class="h-32 w-56 bg-white p-2 rounded-full" src="/images/shputin.jpeg"  />
+                </div>
+    
+                <div class=" ">
+                    <div class="text-center px-14">
+                        <h2 class="font-serif text-gray-800 text-4xl font-bold">Khairil Ilmi</h2>
+                        <h2 class="text-gray-700 text-2xl font-bold">(2108107010011)</h2>
+                        <a class="font-bold text-gray-600 mt-2 hover:text-blue-500" href="https://www.instagram.com/immohitdhiman/" target="BLANK()">@bg_putin.official</a>
+                        <p class="font-serif mt-2 text-gray-700 font-semibold text-sm">sesungguhnya berkasih lah di antara manusia perindah segala kata kata bahagia informatika</p>
+                    </div>
                 </div>
             </div>
+    
         </div>
-
-    </div>
-
-    <!--foto nizar dan riyan-->    
-    <div class="h-screen  bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
-        <!--foto nizar-->
-        <div class="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
-            <div class=" h-56 overflow-hidden -mb-24 " >
-                <img class="" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"/>
-            </div>
-            <div class="flex justify-center px-5 ">
-                <img class="h-32 w-56 bg-white p-2 rounded-full" src="/images/shputin.jpeg"  />
-            </div>
-
-            <div class=" ">
-                <div class="text-center px-14">
-                    <h2 class="font-serif text-gray-800 text-4xl font-bold">M. Nizar Asykary</h2>
-                    <h2 class="text-gray-700 text-2xl font-bold">(2108107010043)</h2>
-                    <a class="font-bold text-gray-600 mt-2 hover:text-blue-500" href="https://www.instagram.com/_asynzr/" target="BLANK()">@_asynzr</a>
-                    <p class="font-serif mt-2 text-gray-700 font-semibold text-sm -mb-2">Tiada yang lebih jago nana selain dina,
-                                                                                            Tiada yg lebih berembun daripada kacamata nitiya,
-                                                                                            Tiada youtuber yang keren selain ilmi,
-                                                                                            Tiada yang menyukai one piece selain nizar,
-                                                                                            Tiada yang lebih cerah daripada senyuman riyan.</p>
-                    <p class="font-serif mt-2 text-gray-700 font-semibold text-sm -mb-2">#fullsenyum</p>
-                    <p class="font-serif mt-2 text-gray-700 font-semibold text-sm ">Klp 2, jaya³</p>
+    
+        <!--foto nizar dan riyan-->    
+        <div class="h-screen   bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
+            <!--foto nizar-->
+            <div class="container mt-28 lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
+                <div class=" h-56 overflow-hidden -mb-24 " >
+                    <img class="" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"/>
+                </div>
+                <div class="flex justify-center px-5 ">
+                    <img class="h-32 w-56 bg-white p-2 rounded-full" src="/images/shputin.jpeg"  />
+                </div>
+    
+                <div class=" ">
+                    <div class="text-center px-14">
+                        <h2 class="font-serif text-gray-800 text-4xl font-bold">M. Nizar Asykary</h2>
+                        <h2 class="text-gray-700 text-2xl font-bold -mb-1">(2108107010043)</h2>
+                        <a class="font-bold text-gray-600 mt-2 hover:text-blue-500 " href="https://www.instagram.com/_asynzr/" target="BLANK()">@_asynzr</a>
+                        <div class="-mt-2">
+                            <p class="font-serif mt-2 text-gray-700 font-semibold text-sm -mb-2">Tiada yang lebih jago nana selain dina,
+                                                                                                    Tiada yg lebih berembun daripada kacamata nitiya,
+                                                                                                    Tiada youtuber yang keren selain ilmi,
+                                                                                                    Tiada yang menyukai one piece selain nizar,
+                                                                                                    Tiada yang lebih cerah daripada senyuman riyan.</p>
+                            <p class="font-serif mt-2 text-gray-700 font-semibold text-sm -mb-2">#fullsenyum</p>
+                            <p class="font-serif mt-2 text-gray-700 font-semibold text-sm ">Klp 2, jaya³</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        
-        <!--foto riyan-->
-        <div class="container ml-20 lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
-            <div class=" h-56 overflow-hidden -mb-24 " >
-                <img class="" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"/>
-            </div>
-            <div class="flex justify-center px-5 ">
-                <img class="h-32 w-56 bg-white p-2 rounded-full" src="/images/shputin.jpeg"  />
-            </div>
-
-            <div class=" ">
-                <div class="text-center px-14">
-                    <h2 class="font-serif text-gray-800 text-4xl font-bold">Bg putin</h2>
-                    <h2 class="text-gray-700 text-2xl font-bold">(21081070100)</h2>
-                    <a class="font-bold text-gray-600 mt-2 hover:text-blue-500" href="https://www.instagram.com/immohitdhiman/" target="BLANK()">@bg_putin.official</a>
-                    <p class="font-serif mt-2 text-gray-700 font-semibold text-sm">Hidup seperti larry</p>
+            
+            <!--foto riyan-->
+            <div class="container ml-20 lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
+                <div class=" h-56 overflow-hidden -mb-24 " >
+                    <img class="" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"/>
+                </div>
+                <div class="flex justify-center px-5 ">
+                    <img class="h-32 w-56 bg-white p-2 rounded-full" src="/images/shputin.jpeg"  />
+                </div>
+    
+                <div class=" ">
+                    <div class="text-center px-14">
+                        <h2 class="font-serif text-gray-800 text-4xl font-bold">Bg putin</h2>
+                        <h2 class="text-gray-700 text-2xl font-bold">(21081070100)</h2>
+                        <a class="font-bold text-gray-600 mt-2 hover:text-blue-500" href="https://www.instagram.com/immohitdhiman/" target="BLANK()">@bg_putin.official</a>
+                        <p class="font-serif mt-2 text-gray-700 font-semibold text-sm">Hidup seperti larry</p>
+                    </div>
                 </div>
             </div>
+    
         </div>
-
     </div>
 </div>
 @endsection

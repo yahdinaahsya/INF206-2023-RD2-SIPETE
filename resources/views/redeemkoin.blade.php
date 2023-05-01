3a4f655434c2f6bc036131df1a6f45ae8c6f5475
Riyanfr <riyan_fr@mhs.unsyiah.ac.id>
2023-05-01 11:09:06 +0700

    feat: atur redeem

--- resources/views/redeemkoin.blade.php
+++ resources/views/redeemkoin.blade.php
@@ -12,7 +12,11 @@
                 <img class="relative pl-16 pt-7 " src="/images/tukarpulsa.jpg">
             </div>
             <div class="h-auto max-w-lg pt-3 pr-16 ml-auto">
-                <img class="relative" src="/images/Close_square_fill.png">
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </button>
             </div>
         </div>
 

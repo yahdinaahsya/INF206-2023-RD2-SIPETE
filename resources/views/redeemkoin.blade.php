da75b9dc8bad9b367442bb2a350edb72e0935483
Riyanfr <riyan_fr@mhs.unsyiah.ac.id>
2023-05-11 00:25:29 +0700

    feat: atur modal bagian 1

--- resources/views/redeemkoin.blade.php
+++ resources/views/redeemkoin.blade.php
@@ -39,7 +39,7 @@
                             <!-- Modal body (bros kain, scrunchie, keset, boneka kain perca, selimut , sarung bantal) -->
                             <div class=" p-6 space-y-6">
                                 
-                                <!-- bagian 1 -->
+                                <!-- bagian redeem 1 -->
                                 <!-- broskain -->
                                 <div class="flex pl-5 space-x-10  ">
                                     <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -57,31 +57,37 @@
                                                         <a class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Tukarkan</a>
                                                     </button>
 
-                                                    <!-- modal 1 -->
-                                                    <div id="tukar" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                                                        <div class="relative w-full max-w-md max-h-full">
-                                                            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                                                <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="tukar">
-                                                                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                                                    <span class="sr-only">Close modal</span>
-                                                                </button>
-                                                                <div class="p-6 text-center">
-                                                                    <svg aria-hidden="true" class="mx-auto mb-4 text-blue-700 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                                                    <h3 class="mb-5 text-lg font-extrabold tracking-tight text-gray-900 dark:text-white">Apakah Anda Yakin Menukarkan Poin Ini?</h3>
-                                                                    <button data-modal-hide="tukar" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                                                        Yakin
+                                                        <!-- modal tukar 1 -->
+                                                        <div id="tukar" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                                            <div class="relative w-full max-w-md max-h-full">
+                                                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                                                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="tukar">
+                                                                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                                                        <span class="sr-only">Close modal</span>
                                                                     </button>
-                                                                    <button data-modal-hide="tukar" type="button" class="text-white bg-gray-400 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-lg border border-gray-300 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-3 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"> Tidak </button>
+                                                                    <div class="p-6 text-center">
+                                                                        <svg aria-hidden="true" class="mx-auto mb-4 text-blue-700 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                                                        <h3 class="mb-5 text-lg font-extrabold tracking-tight text-gray-900 dark:text-white">Apakah Anda Yakin Menukarkan Poin Ini?</h3>
+                                                                        <button  data-modal-hide="tukar" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                                            Yakin
+                                                                        </button>
+
+                                                                        <button data-modal-hide="tukar" type="button" class="text-white bg-gray-400 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-lg border border-gray-300 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"> Tidak </button>
+                                                                        
+                                                                            
+                                                                            
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                    </div>
 
                                                 </div>
 
                                             </div>
                                         </div>
                                     </div>
+                                    
+
                                     
                                     <!-- scrunchie -->
                                     <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
